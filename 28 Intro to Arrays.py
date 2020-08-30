2a802fec954ff821d9e0bf6ce258b5f37379fccb
CJM <mrcmillington@gmail.com>
2020-08-30 12:08:54 +0100

    mid Change

--- 28 Intro to Arrays.py
+++
@@ -1,7 +0,0 @@
-# Demo ( Copy the code below )
-teams = ["Arsenal","Man Utd","Liverpool","AFC"]
-print(teams[1])
-for tm in teams:
-    print(tm)
-
-
--- 41 Read File.py
+++
@@ -1,6 +0,0 @@
-# Demo ( Copy the code below )
-f = open("41 africa.txt","r")
-print(f.read())
-
-
-
